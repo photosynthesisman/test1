757e1549919547a32df8e5d80e589143e22fe3c5
anoju <zun5761@gmail.com>
2024-01-31 09:57:38 +0900

    include 경로 추가 및 top버튼은 footer.js 속으로

--- html/include/footer.js
+++ html/include/footer.js
@@ -31,7 +31,8 @@ function includeFooter() {
     </div>
     <span class="copyright">&copy; 2024 HiUX Consulting. All Rights Reserved.</span>
   </div>
-</footer>`;
+</footer>
+<div class="scroll-top"><button type="button" class="btn-scroll-top" aria-label="페이지 상단으로 이동"></button></div>`;
   document.write(str);
   const $include = document.querySelector('.__include');
   if ($include) $include.remove();
--- html/include/header.js
+++ html/include/header.js
@@ -2,7 +2,7 @@ function includeHeader() {
   let str = `
 <header id="header" class="header">
   <div class="inner">
-    <h1 class="h1"><a href="main.html" role="button" aria-label="홈페이지 바로가기">하이유엑스컨설팅</a></h1>
+    <h1 class="h1"><a href="main.html" aria-label="홈페이지 바로가기">하이유엑스컨설팅</a></h1>
     <div class="right">
       <button type="button" aria-label="메뉴 열기" class="btn-menu">
         <strong>MENU</strong>
@@ -10,12 +10,12 @@ function includeHeader() {
       </button>
       <nav class="commonGnb">
         <ol>
-          <li class="menu-li"><a href="#none" role="button">About</a></li>
-          <li class="menu-li"><a href="#none" role="button">Work</a></li>
-          <li class="menu-li"><a href="#none" role="button">News</a></li>
-          <li class="menu-li"><a href="#none" role="button">Contact</a></li>
+          <li class="menu-li"><a href="about.html">About</a></li>
+          <li class="menu-li"><a href="work.html">Work</a></li>
+          <li class="menu-li"><a href="news.html">News</a></li>
+          <li class="menu-li"><a href="contact.html">Contact</a></li>
         </ol>
-        <button type="button" aria-label="본사소개서 다운로드" class="btn-download">Company Brief</button>
+        <a href="#" type="button" aria-label="본사소개서 다운로드" class="btn-download">Company Brief</a>
       </nav>
     </div>
   </div>
