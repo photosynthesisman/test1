8e2448fd097494347d25a9566c6ec85c875d254c
anoju <zun5761@gmail.com>
2024-01-31 10:11:05 +0900

    map 보완

--- static/js/common.js
+++ static/js/common.js
@@ -209,42 +209,38 @@ function mainUI() {
 }
 
 //map
-function mapInit() {
-  var mapLocation = {
+function kakakoMapInit() {
+  const mapContainer = document.getElementById('companyMap');
+  const mapLocation = {
     x: 37.530733879674145,
     y: 126.89887339311068
   };
-  var mapContainer1 = document.getElementById('companymap');
-  var mapOptions1 = {
+  const mapOptions = {
     center: new kakao.maps.LatLng(mapLocation.x, mapLocation.y),
     level: 3
   };
-  var map1 = new kakao.maps.Map(mapContainer1, mapOptions1);
+  const map = new kakao.maps.Map(mapContainer, mapOptions);
 
-  var markerPosition = new kakao.maps.LatLng(mapLocation.x, mapLocation.y);
-
-  var imageSrc = '../static/images/common/map_marker.png'; // 마커이미지의 주소
-  var imageSize;
-  var imageOption1;
+  const imageSrc = '../static/images/common/map_marker.png'; // 마커이미지의 주소
+  let imageSize;
+  let imageOption;
   if (window.innerWidth < 800) {
     imageSize = new kakao.maps.Size(55, 64); // 마커이미지의 크기
-    imageOption1 = {
-      offset: new kakao.maps.Point(28, 64)
-    }; // 마커이미지의 옵션
+    imageOption = {
+      offset: new kakao.maps.Point(27.5, 64)
+    };
   } else {
     imageSize = new kakao.maps.Size(110, 128); // 마커이미지의 크기
-    imageOption1 = {
+    imageOption = {
       offset: new kakao.maps.Point(55, 128)
-    }; // 마커이미지의 옵션
+    };
   }
-
-  var markerImage1 = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption1),
-    markerPosition1 = new kakao.maps.LatLng(mapLocation.x, mapLocation.y);
-
-  var marker1 = new kakao.maps.Marker({
-    position: markerPosition1,
-    image: markerImage1
+  const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+  const markerPosition = new kakao.maps.LatLng(mapLocation.x, mapLocation.y);
+  const marker = new kakao.maps.Marker({
+    position: markerPosition,
+    image: markerImage
   });
-
-  marker1.setMap(map1);
+  marker.setMap(map);
 }
+kakakoMapInit();
